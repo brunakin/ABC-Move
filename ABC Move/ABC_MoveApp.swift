12fc8e2dcfffbec39a1e1df9869c218cf71cc8cf
Thais <thais.randrade5@senacsp.edu.br>
2024-04-19 14:55:58 -0300

    Initial Commit

---
+++ ABC Move/ABC_MoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABC_MoveApp.swift
+//  ABC Move
+//
+//  Created by THAIS RODRIGUES ANDRADE on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ABC_MoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
